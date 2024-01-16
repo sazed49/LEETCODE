4d7939eaad494d9b46ab6c75707cbe4baa3420e5
mdsazedurrahman <sazedcse17@gmail.com>
2024-01-16 01:29:25 -0600

    new problem solved

---
+++ contain_duplicateII.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        for i in range(len(nums)):
+            print({"i"},i)
+
+            print({"num"},nums[i])
+            print({"i+k"},i+k)
+            print({"len"},len(nums))
+            if (i+k)>len(nums):
+                return False
+            elif nums[i+k]==nums[i]:
+                return True
+            else: continue
+        return False
+
+
+
+
+sol=Solution()
+nums=[1,2,3,1,2,3]
+k=2
+ans=sol.containsNearbyDuplicate(nums,k)
+print(ans)
